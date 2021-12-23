02dd9f6116c6836504e83fbaff66a31dfd6ca745
choco-tyranno <choco.tyranno@gmail.com>
2021-12-24 01:53:27 +0900

    feat: add AES256UtilTest.kt

---
+++ TodoApp/app/src/androidTest/java/com/choco_tyranno/todoapp/AES256UtilTest.kt
@@ -0,0 +1,18 @@
+package com.choco_tyranno.todoapp
+
+import android.util.Log
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.choco_tyranno.todoapp.data.util.AES256Util
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.util.*
+
+@RunWith(AndroidJUnit4::class)
+class AES256UtilTest {
+    @Test
+    fun encrypt(){
+        val randomId = UUID.randomUUID().toString()
+        val encryptedId = AES256Util.instance.encrypt(randomId)
+        Log.d("@@Test","randomId:$randomId /  encryptedId: $encryptedId")
+    }
+}
---
+++ TodoApp/app/src/main/java/com/choco_tyranno/todoapp/data/util/AES256Util.kt
@@ -0,0 +1,53 @@
+package com.choco_tyranno.todoapp.data.util
+
+import com.google.android.gms.common.util.Base64Utils
+import java.security.MessageDigest
+import java.security.SecureRandom
+import java.util.*
+import javax.crypto.Cipher
+import javax.crypto.KeyGenerator
+import javax.crypto.SecretKey
+import javax.crypto.spec.IvParameterSpec
+
+class AES256Util private constructor(){
+    private val keygen = KeyGenerator.getInstance("AES")
+    private fun createKey():SecretKey{
+        keygen.init(256)
+        return keygen.generateKey()
+    }
+
+    fun encrypt(input: String): String {
+        val key = createKey()
+        val arr = ByteArray(16)
+        var str = ""
+        while (str.length != 16) {
+            SecureRandom().nextBytes(arr)
+            str = String(arr)
+        }
+        val iv = getIv(str)
+        val cipher: Cipher = Cipher.getInstance("AES/GCM/NoPadding")
+        cipher.init(Cipher.ENCRYPT_MODE, key, iv)
+        val encrypt = cipher.doFinal(input.toByteArray())
+        return str + Base64Utils.encode(encrypt)
+    }
+
+        private fun getIv(input: String): IvParameterSpec {
+        val ivHash = MessageDigest.getInstance("SHA1")
+            .digest(input.toByteArray())
+        val ivBytes = Arrays.copyOf(ivHash, 16)
+        return IvParameterSpec(ivBytes)
+    }
+
+//    private fun decrypt(input: String, key: SecretKey): String {
+//        val iv = getIv(input.substring(0, 16))
+//        val cryptText = input.substring(16, input.length)
+//        val cipher = Cipher.getInstance("AES/GCM/NoPadding")
+//        cipher.init(Cipher.DECRYPT_MODE, key, iv)
+//        val decrypt = cipher.doFinal(Base64Utils.decode(cryptText))
+//        return String(decrypt)
+//    }
+
+    companion object{
+        val instance :AES256Util = AES256Util()
+    }
+}
