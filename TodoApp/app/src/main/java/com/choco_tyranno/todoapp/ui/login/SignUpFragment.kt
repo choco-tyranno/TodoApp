aa6d17a7e3df1517b4de3b4002926a06e4133ed4
choco-tyranno <choco.tyranno@gmail.com>
2021-12-24 13:22:10 +0900

    feat: add sign up with duplicate email checking methods and adding email to db methods

--- TodoApp/app/src/main/java/com/choco_tyranno/todoapp/ui/login/SignUpFragment.kt
+++ TodoApp/app/src/main/java/com/choco_tyranno/todoapp/ui/login/SignUpFragment.kt
@@ -1,10 +1,9 @@
 package com.choco_tyranno.todoapp.ui.login
 
-import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
-import android.widget.Toast
+import android.widget.ProgressBar
 import androidx.appcompat.widget.AppCompatEditText
 import androidx.core.widget.addTextChangedListener
 import com.choco_tyranno.todoapp.R
@@ -12,15 +11,18 @@ import com.choco_tyranno.todoapp.data.models.User
 import com.choco_tyranno.todoapp.data.util.HashProvider
 import com.choco_tyranno.todoapp.ui.login.view.SignUpSubmitView
 import com.choco_tyranno.todoapp.ui.login.view.SignUpValueResultView
+import com.choco_tyranno.todoapp.ui.univ.ToastManager
 import com.google.android.material.button.MaterialButton
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.DocumentReference
+import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.toObject
 import com.google.firebase.ktx.Firebase
 import dagger.hilt.android.AndroidEntryPoint
 import java.util.*
 import java.util.regex.Pattern
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class SignUpFragment : Fragment(R.layout.fragment_signup) {
@@ -33,9 +35,9 @@ class SignUpFragment : Fragment(R.layout.fragment_signup) {
     lateinit var passwordDoubleCheckResult: SignUpValueResultView
     lateinit var submit: SignUpSubmitView
     lateinit var cancel: MaterialButton
+    lateinit var loading: ProgressBar
     private lateinit var emailRegex: String
     private lateinit var passwordRegex: String
-    private var toast: Toast? = null
     private lateinit var auth: FirebaseAuth
     private lateinit var db: FirebaseFirestore
 
@@ -47,6 +49,7 @@ class SignUpFragment : Fragment(R.layout.fragment_signup) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        loading = view.findViewById<ProgressBar>(R.id.signUpFragment_loading)
         emailRegex = view.resources.getString(R.string.signUp_emailRegexPattern)
         passwordRegex = view.resources.getString(R.string.signUp_passwordRegexPattern)
         email = view.findViewById<AppCompatEditText>(R.id.signUpFragment_emailInput)
@@ -70,7 +73,6 @@ class SignUpFragment : Fragment(R.layout.fragment_signup) {
         passwordDoubleCheckResult = view.findViewById(R.id.signUpFragment_passwordDoubleCheckResult)
         submit = view.findViewById<SignUpSubmitView>(R.id.signUp_submit)
         submit.setOnClickListener {
-            toast(view.context, "submit!")
             signUp()
         }
         cancel = view.findViewById<MaterialButton>(R.id.signUp_cancel)
@@ -84,19 +86,70 @@ class SignUpFragment : Fragment(R.layout.fragment_signup) {
         )
     }
 
+    private fun toggleLoading() {
+        when (loading.visibility) {
+            View.VISIBLE -> loading.visibility = View.INVISIBLE
+            View.INVISIBLE -> loading.visibility = View.VISIBLE
+            View.GONE -> {}
+        }
+    }
+
+    private fun popupDuplicateEmailMessage() {
+        toggleLoading()
+        ToastManager.toast(email.context, "중복된 이메일입니다. 다른 이메일로 시도해주세요.")
+    }
+
     private fun signUp() {
-        db.collection("users")
-            .document(UUID.randomUUID().toString())
-            .set(
-                User(email.text.toString()
-                    , HashProvider.hashSHA256(password.text.toString())
-                ))
-            .addOnSuccessListener {
-                toast(email.context, "success!")
+        toggleLoading()
+        checkDuplicateEmailWithFirebase()
+    }
+
+    private fun checkDuplicateEmailWithFirebase() {
+        fun createAccount() {
+            val context = email.context
+            auth.createUserWithEmailAndPassword(
+                email.text.toString(), HashProvider.hashSHA256(password.text.toString())
+            )
+                .addOnCompleteListener { result ->
+                    if (result.isSuccessful) {
+                        //성공
+                    }
+                    if (result.isCanceled) {
+                        ToastManager.toast(
+                            context,
+                            context.resources.getString(R.string.signUp_signUpRequestFail)
+                        )
+                    }
+                }
+        }
+
+        fun requestToAddEmail(emailRef: DocumentReference) {
+            val context = email.context
+            emailRef.update("email_list", FieldValue.arrayUnion(email.text.toString()))
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        createAccount()
+                    } else if (task.isCanceled) {
+                        ToastManager.toast(
+                            context,
+                            context.resources.getString(R.string.signUp_signUpRequestFail)
+                        )
+                    }
+                    toggleLoading()
+                }
+        }
+
+        val emailRef = db.collection("users").document("emails")
+        emailRef.get().addOnSuccessListener { documentSnapshot ->
+            val emailList = documentSnapshot.toObject<EmailList>()
+            val alreadyExist = emailList?.alreadyExist(email.text.toString())
+            if (alreadyExist == true) {
+                popupDuplicateEmailMessage()
+                return@addOnSuccessListener
             }
-            .addOnFailureListener {
-                toast(email.context, "fail..")
-            }
+            // add email to email_list
+            requestToAddEmail(emailRef)
+        }
     }
 
     private fun isEmailValid(): Boolean {
@@ -140,13 +193,10 @@ class SignUpFragment : Fragment(R.layout.fragment_signup) {
             passwordFormResult.onInvalidValue()
         }
     }
+}
 
-    private fun toast(context: Context, msg: String) {
-        val hotToast = Toast.makeText(context, msg, Toast.LENGTH_SHORT)
-        if (toast != null) {
-            toast?.cancel()
-        }
-        toast = hotToast
-        toast?.show()
+data class EmailList(val list: List<String>? = null) {
+    fun alreadyExist(email: String): Boolean {
+        return list?.contains(email) == true
     }
 }
